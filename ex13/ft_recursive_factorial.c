b074a1516a1b4f50c30ffc620d66ed5d63ca32d1
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-10-30 14:11:15 +0300

    ex13 overflow handled

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 08:58:14 by thakala           #+#    #+#             */
+/*   Updated: 2021/10/30 14:09:18 by thakala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb == 0)
+		return (1);
+	else if (nb < 0 || nb > 12)
+		return (0);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
